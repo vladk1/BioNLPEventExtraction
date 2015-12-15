f5f0e06b10dd974ce2bd2497fc8c254707040852
Джон <kolesnik-vlad@mail.ru>
2015-12-15 12:28:21 +0000

    NB trigger features 0.285

--- Problem3.scala
+++ Problem3.scala
@@ -1,8 +1,5 @@
 package uk.ac.ucl.cs.mr.statnlpbook.assignment2
 
-import scala.collection.immutable.ListMap
-import scala.collection.mutable
-
 /**
  * Created by Georgios on 06/11/2015.
  */
@@ -44,21 +41,22 @@ object Problem3Triggers {
     // define model
     //TODO: change the features function to explore different types of features
 //    val triggerModel = SimpleClassifier(triggerLabels, Features.defaultTriggerFeatures)
-    val triggerModel = SimpleClassifier(triggerLabels, Features.myTriggerFeatures)
+    val triggerModel = SimpleClassifier(triggerLabels, Features.myNBTriggerFeatures)
+//    val triggerModel = SimpleClassifier(triggerLabels, Features.myTriggerFeatures)
 
 
     // use training algorithm to get weights of model
     //TODO: change the trainer to explore different training algorithms
-    //    val triggerWeights = PrecompiledTrainers.trainNB(triggerTrain,triggerModel.feat)
-    val triggerWeights = PrecompiledTrainers.trainPerceptron(triggerTrain, triggerModel.feat, triggerModel.predict, 10)
+        val triggerWeights = PrecompiledTrainers.trainNB(triggerTrain,triggerModel.feat)
+//    val triggerWeights = PrecompiledTrainers.trainPerceptron(triggerTrain, triggerModel.feat, triggerModel.predict, 10)
 
 //    trick to print out weights with mention of word
-    val sortedWeights = ListMap(triggerWeights.toSeq.sortBy(_._2):_*)
-    sortedWeights.foreach(weight => {
-      if (weight._1.toString.contains("syntactic dependency")) {
-        println(weight)
-      }
-    })
+//    val sortedWeights = ListMap(triggerWeights.toSeq.sortBy(_._2):_*)
+//    sortedWeights.foreach(weight => {
+//      if (weight._1.toString.contains("candidate token")) {
+//        println(weight)
+//      }
+//    })
 
 
     // get predictions on dev
